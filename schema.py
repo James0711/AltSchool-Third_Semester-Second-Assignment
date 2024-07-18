7fe4740fa8ef8022ac91dbfdc6adc8d1c87be9e6
James0711 <jengreat94@gmail.com>
2024-07-18 23:30:54 +0100

    Initial Commit

---
+++ schema.py
@@ -0,0 +1,33 @@
+from typing import Optional
+from pydantic import BaseModel
+from bson.objectid import ObjectId
+ 
+class BookBase(BaseModel):
+    title: str
+    author: str
+    description: str
+
+class Book(BookBase):
+    id: str
+        
+class BookCreatePayload(BookBase):
+    pass
+
+class BookCreate(BookBase):
+    user_id: str
+
+class BookUpdate(BookBase):
+    description: Optional[str] = None
+
+# User
+class UserBase(BaseModel):
+    id: str
+    username: str
+
+class UserCreate(UserBase):
+    full_name: str
+    password: str
+
+class UserInDB(UserBase):
+    full_name: str
+    hashed_password: str
